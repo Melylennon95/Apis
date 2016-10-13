459530534d2ff62b2ae8d158152abcc263ccd3ae
JavierCruz94 <javiercruz_94@hotmail.com>
2016-10-13 17:59:35 -0500

    Mas Relaciona

--- PrepaNet/Assets/Scripts/SeleccionaRespuesta.cs
+++ PrepaNet/Assets/Scripts/SeleccionaRespuesta.cs
@@ -8,6 +8,32 @@ public class SeleccionaRespuesta : MonoBehaviour{
 
 	public void changeState() {
 		clickState = !clickState;
+
+		CambiaColor();
+	}
+
+	public void CambiaColor() {
+		if (clickState) {
+			ChangeColor (0.0f, 255.0f, 23.0f, 255.0f);
+		} else {
+			ChangeColor (255.0f, 255.0f, 255.0f, 255.0f);
+		}
+	}
+
+	public void ColorEncima() {
+		ChangeColor (0.0f, 255.0f, 0.0f, 0.3f);
+	}
+
+	public void ColorSale() {
+		if (clickState)
+			ChangeColor (0.0f, 255.0f, 23.0f, 255.0f);
+		else
+			ChangeColor (255.0f, 255.0f, 255.0f, 255.0f);
+	}
+
+	public void ChangeColor(float a, float b, float c, float d) {
+		Color color = new Color (a,b,c,d);
+		this.gameObject.GetComponent<Text> ().color = color;
 	}
 
 		
